9e8208bbe55461508483d1eb24c0589ff39379e5
Rafael Klein <rafael.augustoklein04@gmail.com>
2025-02-21 18:58:18 -0300

    feat: implementando ultimas rotas

--- src/controllers/subscribers/subscribers_manager.py
+++ src/controllers/subscribers/subscribers_manager.py
@@ -33,7 +33,8 @@ class SubscriberManager:
                         "count": len(formatted_subscriber),
                         "subscribers": formatted_subscriber
                     }
-                }
+                },
+                status_code=200
             )
         
     def __format_event_ranking(self, event_ranking: list) -> HttpResponse:
@@ -52,5 +53,6 @@ class SubscriberManager:
                         "count": len(formatted_event_ranking),
                         "ranking": formatted_event_ranking
                     }
-                }
+                },
+                status_code=200
             )
